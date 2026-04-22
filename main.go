4e1fa424be9599fb354ac276a74207345ec2d5cd
jgr0sz <243434564+jgr0sz@users.noreply.github.com>
2026-04-22 15:44:08 -0400

    Added concurrency to connection handling, refactored code significantly and rewrote misleading comments

--- main.go
+++ main.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"bufio"
-	"fmt"
+	"log"
 	"net"
 	"strings"
 )
 
 // "macro" consts for common status codes
 const (
-	OK_GET      = "HTTP/1.1 200 OK\r\n\r\n"
-	BAD_GET     = "HTTP/1.1 404 Not Found\r\n\r\n"
+	OK = "HTTP/1.1 200 OK\r\n\r\n"
+	NOT_FOUND = "HTTP/1.1 404 Not Found\r\n\r\n"
 	NOT_ALLOWED = "HTTP/1.1 405 Method Not Allowed\r\n\r\n"
 )
+
+// Defines a serverside response to an HTTP request after performing error checking
+func writeResponse(conn net.Conn, response string) {
+	_, err := conn.Write([]byte(response))
+	if err != nil {
+		log.Println("Error writing response: ", err)
+	}
+}
 
 // Extracts the first line of the HTTP request to determine its type/validity
 func connHandler(conn net.Conn) {
@@ -22,50 +30,54 @@ func connHandler(conn net.Conn) {
 	reader := bufio.NewReader(conn)
 	line, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Print("Error reading request: ", err)
+		log.Println("Error reading request: ", err)
+		return
 	}
 
 	//Splits header into sections which we will utilize
 	headerSections := strings.Fields(line)
 	if len(headerSections) < 3 {
-		fmt.Print("Malformed request: ", line)
+		log.Println("Malformed request: ", line)
+		return
 	}
 
+	//GET request case (rudimentary)
 	if headerSections[0] == "GET" {
-		conn.Write([]byte(strings.Join([]string{OK_GET, "Here is a GET request"}, " ")))
+		writeResponse(conn, OK + "GET Response of some kind....")
 	} else {
-		conn.Write([]byte(strings.Join([]string{NOT_ALLOWED, "Request type not allowed..."}, " ")))
+		writeResponse(conn, NOT_ALLOWED + "Response type not allowed.")
 	}
 }
 
 // Main listener function for TCP connections to the server
 func listenTCP() {
-	//Listening for a TCP response
-	response, err := net.Listen("tcp", "127.0.0.1:9999")
+	//TCP listener creation
+	listener, err := net.Listen("tcp", "127.0.0.1:9999")
 
-	//If a response was found, output it, output any error otherwise
+	//Error check
 	if err != nil {
-		fmt.Print("Error while listening ", err)
-		return
+		log.Fatal("Error while listening ", err)
 	}
+
+	//Logging listener status
+	log.Printf("\nListening on\n------------\nType: %s\nAddress: %s\n",
+	listener.Addr().Network(),
+	listener.Addr().String())
 	//deferred statement to close our listener at the end of the function
-	defer response.Close()
+	defer listener.Close()
 
-	//Message to indicate connection
-	fmt.Printf("Connected\n-------\nType: %s\nAddress: %s",
-		response.Addr().Network(),
-		response.Addr().String())
-
-	conn, err := response.Accept()
-	if err != nil {
-		fmt.Print("Unable to accept connection ", err)
+	//Loop that blocks on Accept() until a connection is found. After error checking, the connection
+	//is handled concurrently through a goroutine.
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Unable to accept connection ", err)
+			continue
+		}
+		go connHandler(conn)
 	}
-	connHandler(conn)
 }
 
 func main() {
-	//Gradually increase wait time after listens
-	for {
-		listenTCP()
-	}
+	listenTCP()
 }
